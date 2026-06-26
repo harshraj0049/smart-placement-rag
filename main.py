8cd03e1ff7541cfb25ee03c26365778dde19eafd
Harsh Raj <harshraj.hr08@gmail.com>
2026-06-26 17:51:31 +0530

    Feat:Added document loading and chuncking

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from smart-placement-rag!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ rag/loader.py
@@ -0,0 +1,28 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+
+
+def pdf_load_and_chunk(pdf_path:str,chunk_size:int =500,chunk_overlap:int =50):
+    loader=PyPDFLoader(pdf_path)
+    docs=loader.load()
+
+    print(f"pages in the document:{len(docs)}")
+    for pages in docs:
+        print(pages.page_content,"\n")
+
+    splitter=RecursiveCharacterTextSplitter(chunk_size=chunk_size,chunk_overlap=chunk_overlap,length_function=len)
+    chunks=splitter.split_documents(docs)
+
+    print(f"Number of chunks : {len(chunks)}\n")
+    for i,chunk in enumerate(chunks):
+        print("chunk number",i,chunk.page_content)
+        print("metadata",chunk.metadata)
+
+    return chunks
+
+
+if(__name__=="__main__"):
+    pdf_load_and_chunk("data/T&P Regulations_2027 Batch.pdf",1000,200)
+
+
